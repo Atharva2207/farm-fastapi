55f50ae3601dfd227af8e558c3e999df34746287
Siddhant-Nawale <siddhant.nawale@gmail.com>
2025-07-11 18:38:12 +0530

    feat: downlaod report api taking file path from param

--- codebase/apis/report.py
+++ codebase/apis/report.py
@@ -9,22 +9,21 @@ route = APIRouter(prefix="/report", tags=["Report"])
 # Initialize boto3 client
 s3_client = boto3.client("s3")
 
-
 @route.post("/download-report")
-def download_report():
+def download_report(key: str = Query(default="reports/report.pdf")):
     bucket_name = "c9s-tiff-files"
-    key = "reports/report.pdf"
 
     try:
         s3_object = s3_client.get_object(Bucket=bucket_name, Key=key)
         file_stream = BytesIO(s3_object['Body'].read())
         content_type = s3_object.get('ContentType', 'application/pdf')
+        filename = key.split("/")[-1]
 
         return StreamingResponse(
             file_stream,
             media_type=content_type,
             headers={
-                "Content-Disposition": f'attachment; filename="report.pdf"'
+                "Content-Disposition": f'attachment; filename="{filename}"'
             }
         )
 
